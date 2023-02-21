1a80618346eadac2c34038b029ee1d5d16d5efdc
Daniel <danielwandurua@gmail.com>
2023-02-22 01:07:07 +0300

    solve profile issue

--- author_api/settings/local.py
+++ author_api/settings/local.py
@@ -12,6 +12,7 @@ SECRET_KEY = env(
 
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8080','http://127.0.0.1:8080']
 # email backend
 EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
 EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
--- author_api/settings/production.py
+++ author_api/settings/production.py
@@ -8,3 +8,4 @@ DJANGO_DEBUG = False
 SECRET_KEY = 'django-insecure-kk9hq9+psl0%76ce00ba2rd9ohxf(l2lqn2i8b2!ik&po8_8px'
 
 ALLOWED_HOSTS = []
+CSRF_TRUSTED_ORIGINS = []
